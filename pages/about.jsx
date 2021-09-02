ba6ce06eb33d367794ab408e1fb89098cc1c5a1f
Jhonny Quispe <jhonnyq17@gmail.com>
2021-09-02 15:56:02 -0400

    primer commit

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+export default function about() {
+  return (
+    <Layout
+      title="About | Next.js"
+      description="Descripción"
+    >
+      <h1>About</h1>
+    </Layout>
+  )
+}
